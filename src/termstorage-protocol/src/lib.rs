59855cd8fa77da67c8638f49a45accf3ef15097f
imaqtkatt <eduarda.so@proton.me>
2023-09-21 11:25:53 -0300

    refactor(protocol): use byteorder crate

--- src/termstorage-protocol/src/lib.rs
+++ src/termstorage-protocol/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod response;
 
-use std::io::{self, BufWriter, Read, Write};
-
-use std::io::Result;
+use std::io::{self, Read, Result, Write};
 
 use termstorage_term::{self, Term};
+
+use byteorder::{NetworkEndian, ReadBytesExt, WriteBytesExt};
 
 #[derive(Debug)]
 pub struct Fetch {
@@ -33,45 +33,39 @@ const TAG_FETCH: u8 = 10;
 const TAG_SET: u8 = 11;
 const TAG_DELETE: u8 = 12;
 
-/// Encodes the given protocol.
+/// Encodes the given Protocol.
 pub fn encode(prot: Protocol) -> Result<Vec<u8>> {
   let mut buf = Vec::new();
 
-  let mut writer = BufWriter::new(&mut buf);
-
   match prot {
     Protocol::Fetch(Fetch { name }) => {
-      writer.write(&[TAG_FETCH])?;
-      write_name(&name, &mut writer)?;
+      buf.write_u8(TAG_FETCH)?;
+      write_name(&name, &mut buf)?;
     }
     Protocol::Set(Set { name, payload }) => {
-      writer.write(&[TAG_SET])?;
-      write_name(&name, &mut writer)?;
+      buf.write_u8(TAG_SET)?;
+      write_name(&name, &mut buf)?;
 
       let payload = termstorage_term::encode(&payload)?;
       let payload_size = payload.len().to_be_bytes();
 
-      writer.write(&payload_size)?;
-      writer.write_all(&payload)?;
+      buf.write(&payload_size)?;
+      buf.write_all(&payload)?;
     }
     Protocol::Delete(Delete { name }) => {
-      writer.write(&[TAG_DELETE])?;
-      write_name(&name, &mut writer)?;
+      buf.write_u8(TAG_DELETE)?;
+      write_name(&name, &mut buf)?;
     }
   }
 
-  let vec = writer.buffer().to_vec();
-  Ok(vec)
+  Ok(buf)
 }
 
 /// Decodes a Protocol with the given reader.
 pub fn decode(reader: &mut impl Read) -> Result<Protocol> {
-  // let mut reader = BufReader::new(slice);
+  let tag = reader.read_u8()?;
 
-  let mut buf = [0u8; 1];
-  reader.read_exact(&mut buf)?;
-
-  match buf[0] {
+  match tag {
     TAG_FETCH => {
       let name = read_name(reader)?;
 
@@ -81,12 +75,9 @@ pub fn decode(reader: &mut impl Read) -> Result<Protocol> {
     TAG_SET => {
       let name = read_name(reader)?;
 
-      let mut buf_payload_size = [0u8; 8];
-      reader.read_exact(&mut buf_payload_size)?;
+      let payload_size = reader.read_uint::<NetworkEndian>(8)?;
 
-      let payload_size = usize::from_be_bytes(buf_payload_size);
-
-      let mut buf_payload = vec![0u8; payload_size];
+      let mut buf_payload = vec![0u8; payload_size as usize];
       reader.read_exact(&mut buf_payload)?;
 
       let payload = termstorage_term::decode(&buf_payload)?;
@@ -107,22 +98,19 @@ pub fn decode(reader: &mut impl Read) -> Result<Protocol> {
 }
 
 fn write_name(name: &str, writer: &mut impl Write) -> Result<()> {
-  let name_size = name.len().to_be_bytes();
-  writer.write(&name_size)?;
+  let name_len = name.len();
+  writer.write_uint::<NetworkEndian>(name_len as u64, 8)?;
 
   let name = name.as_bytes();
-  writer.write(name)?;
+  writer.write_all(name)?;
 
   Ok(())
 }
 
 fn read_name(reader: &mut impl Read) -> Result<String> {
-  let mut name_size_buf = [0u8; 8];
-  reader.read_exact(&mut name_size_buf)?;
+  let name_size = reader.read_uint::<NetworkEndian>(8)?;
 
-  let name_size = usize::from_be_bytes(name_size_buf);
-
-  let mut name_buf = vec![0u8; name_size];
+  let mut name_buf = vec![0u8; name_size as usize];
   reader.read_exact(&mut name_buf)?;
 
   let name = String::from_utf8(name_buf);
--- src/termstorage-protocol/src/response.rs
+++ src/termstorage-protocol/src/response.rs
@@ -1,6 +1,8 @@
 use termstorage_term::Term;
 
-use std::io::{self, BufWriter, Read, Result, Write};
+use std::io::{self, Read, Result, Write};
+
+use byteorder::{NetworkEndian, ReadBytesExt, WriteBytesExt};
 
 #[derive(Debug)]
 pub enum Response {
@@ -17,44 +19,37 @@ const TAG_NOT_FOUND: u8 = 52;
 const TAG_UNPROCESSED: u8 = 53;
 const TAG_SERVER_ERROR: u8 = 54;
 
-pub fn encode(resp: Response) -> Result<Vec<u8>> {
-  let buf = Vec::new();
+const USIZE_BYTES: usize = 8;
 
-  let mut writer = BufWriter::new(buf);
+pub fn encode(resp: Response) -> Result<Vec<u8>> {
+  let mut buf = Vec::new();
 
   match resp {
     Response::Ok(term) => {
-      writer.write(&[TAG_OK])?;
+      buf.write_u8(TAG_OK)?;
       let payload = termstorage_term::encode(&term)?;
-      let payload_size = payload.len().to_be_bytes();
+      let payload_size = payload.len();
 
-      writer.write(&payload_size)?;
-      writer.write(&payload)?
+      buf.write_uint::<NetworkEndian>(payload_size as u64, USIZE_BYTES)?;
+      buf.write(&payload)?;
     }
-    Response::Processed => writer.write(&[TAG_PROCESSED])?,
-    Response::NotFound => writer.write(&[TAG_NOT_FOUND])?,
-    Response::Unprocessed => writer.write(&[TAG_UNPROCESSED])?,
-    Response::ServerError => writer.write(&[TAG_SERVER_ERROR])?,
+    Response::Processed => buf.write_u8(TAG_PROCESSED)?,
+    Response::NotFound => buf.write_u8(TAG_NOT_FOUND)?,
+    Response::Unprocessed => buf.write_u8(TAG_UNPROCESSED)?,
+    Response::ServerError => buf.write_u8(TAG_SERVER_ERROR)?,
   };
 
-  let vec = writer.buffer().to_vec();
-  Ok(vec)
+  Ok(buf)
 }
 
 pub fn decode(reader: &mut impl Read) -> Result<Response> {
-  // let mut reader = BufReader::new(slice);
+  let tag = reader.read_u8()?;
 
-  let mut buf = [0u8; 1];
-  reader.read_exact(&mut buf)?;
-
-  match buf[0] {
+  match tag {
     TAG_OK => {
-      let mut payload_size_buf = [0u8; 8];
-      reader.read_exact(&mut payload_size_buf)?;
+      let payload_size = reader.read_uint::<NetworkEndian>(USIZE_BYTES)?;
 
-      let payload_size = usize::from_be_bytes(payload_size_buf);
-
-      let mut payload_buf = vec![0u8; payload_size];
+      let mut payload_buf = vec![0u8; payload_size as usize];
       reader.read_exact(&mut payload_buf)?;
 
       let term = termstorage_term::decode(&payload_buf)?;
